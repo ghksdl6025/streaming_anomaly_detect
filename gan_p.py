fc062f70275ade3f5bd58dc164ca27475d6790c4
Suhwan Lee <ghksdl6025@gmail.com>
2021-10-21 17:25:56 +0200

    21.10.2021 update

---
+++ gan_p.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision import dataset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.transforms.functional import to_pil_image
+import matplotlib.pylab as plt
+%matplotlib inline
+
